b9c38e8900fbe7228726f49f943b0538429d609f
Kudashov_dv <kudashov@kursksu.ru>
2017-12-02 23:22:42 +0300

    Созданы 3 компонента (todo, item, fullItem)  и типы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { ItemComponent } from './todo/item/item.component';
+import { FullItemComponent } from './todo/item/full-item/full-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    ItemComponent,
+    FullItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/todo/item/full-item/full-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-full-item',
+  templateUrl: './full-item.component.html',
+  styleUrls: ['./full-item.component.scss']
+})
+export class FullItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
